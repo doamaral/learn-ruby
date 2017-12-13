b1ea8225cc9000cb5a3178a02105361dccbb4e0b
doamaral <lopesdoamaral@gmail.com>
2017-12-13 16:27:41 -0300

    Arrays

---
+++ modulo_2/repeticao.rb
@@ -0,0 +1,17 @@
+
+
+count = 1
+
+while count < 10 #Enquando a condição for Verdadeira
+  puts "Lucas - #{count}"
+  count+=1
+end
+
+until_count = 30
+until until_count <= 1 #Enquando a condição for Falsa
+  puts "contador = #{until_count}"
+  until_count -=1
+end
+
+
+puts "Lucas\n"*10
---
+++ modulo_2/vector_arrays.rb
@@ -0,0 +1,19 @@
+v = []
+
+i = 0
+
+while i < 10
+  v.push(10-i)
+  i+=1
+end
+
+i = 0
+
+while i<10
+  puts v[i]
+  i+=1
+end
+
+pop = v.pop()
+
+puts pop
