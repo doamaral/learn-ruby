d87a366785cfa3bda45e57ff28ccc1fbe472a1d0
doamaral <lopesdoamaral@gmail.com>
2017-12-18 11:43:16 -0300

    bundling our game

--- modulo_4/jogo/teste_fibo_class.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'lib/sortear_numero_arquivo'
-sorteio = SortearNumero.new
-sorteio.init_file('lib/src/fibo_num.txt', 5)
-num = sorteio.get_num_file('lib/src/fibo_num.txt')
-puts num
